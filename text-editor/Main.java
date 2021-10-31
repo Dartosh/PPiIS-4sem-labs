14cb476f5a578d2150b335ce8cdf0451fb7ebebf
Dartosh <dartoshka21@gmail.com>
2021-10-31 20:54:07 +0300

    refactor: replace files

---
+++ text-editor/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    public static void main(String[] args) throws BadLocationException {
+        FrameCreator p = new FrameCreator();
+        p.setProperties();
+    }
+
+
+}
